9d2feb7319d92704cd4aa08b4863cce898d79871
Akash <akashSenshi@gmail.com>
2024-06-20 09:09:07 +0530

    Initial commit

---
+++ src/test/java/com/apnadukaan/productservice/ApnadukaanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apnadukaan.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApnadukaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
