6dbcd23223254ce9040741da47737aacf5baa414
Akash <akashSenshi@gmail.com>
2024-06-22 17:23:25 +0530

    "Implemented and Solved Multiple Implementation problem"

--- src/main/java/com/apnadukaan/productservice/controller/ProductController.java
+++ src/main/java/com/apnadukaan/productservice/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.apnadukaan.productservice.controller;
 import com.apnadukaan.productservice.ExceptionHandler.Exceptions.ProductNotFoundException;
 import com.apnadukaan.productservice.model.Product;
 import com.apnadukaan.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +14,7 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
     ProductService productService;
-    ProductController(ProductService productService) {
+    ProductController(@Qualifier("ProductServiceViaSelf")ProductService productServicegi) {
         this.productService = productService;
     }
 
--- src/main/java/com/apnadukaan/productservice/service/ProductServiceViaFakeStore.java
+++ src/main/java/com/apnadukaan/productservice/service/ProductServiceViaFakeStore.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
+@Service("ProductServiceViaFakeStore")
 public class ProductServiceViaFakeStore implements ProductService {
     private RestTemplate restTemplate;
 
